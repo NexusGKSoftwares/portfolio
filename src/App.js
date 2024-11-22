f272bec87f1c131441c3ad2f660707d6b7b36689
NexusGKSoftwares <nexusgk3@gmail.com>
2024-11-22 18:45:17 +0300

    "Refactored App.js: removed unnecessary imports, reformatted code, and simplified router configuration"

--- src/App.js
+++ src/App.js
@@ -1,50 +1,39 @@
-
 import './App.css';
 
-// import {Header} from "./Componant/Header/Header"
-import {Hero} from "./Componant/Hero/Hero"
-import {Main} from "./Componant/Main/Main"
-import {ContactUs} from "./Componant/ContactUs/ContactUs"
-// import {Footer} from "./Componant/Footer/Footer"
-import  {Notfound} from "./Componant/Notfound/Notfound"
-import {Layout} from './Componant/Layout/Layout';
-import {RouterProvider, createBrowserRouter} from 'react-router-dom';
+// Import components
+import { Hero } from './Componant/Hero/Hero';
+import { Main } from './Componant/Main/Main';
+import { ContactUs } from './Componant/ContactUs/ContactUs';
+import { Notfound } from './Componant/Notfound/Notfound';
+import { Layout } from './Componant/Layout/Layout';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Skills from './Componant/Skils/Skills';
 import Resume from './Componant/Resume/Resume';
 import ProjectDetails from './Componant/ProjectDetails/ProjectDetails';
 
-
-
 function App() {
+  const routers = createBrowserRouter([
+    {
+      path: '/',
+      element: <Layout />,
+      children: [
+        { path: 'Gideon-Portfolio', element: <Hero /> },
+        { path: 'contactus', element: <ContactUs /> },
+        { path: 'main', element: <Main /> },
+        { path: 'skills', element: <Skills /> },
+        { path: '*', element: <Notfound /> },
+        { path: 'hero', element: <Hero /> },
+        { path: 'projectDetails/:id', element: <ProjectDetails /> },
+        { path: 'resume', element: <Resume /> },
+      ],
+    },
+  ]);
 
-  let routers = createBrowserRouter([
-    {path : "/" , element : <Layout/> , children:[
-      {path : "Gideon-Portfolio" , element :<Hero/>},
-      {path : "contactus" , element :<ContactUs/>},
-      {path : "main" , element :<Main/>},
-      {path : "skills" , element :<Skills/>},
-      {path : "*" , element :<Notfound/>},
-      {path : "hero" , element :<Hero/>},
-      {path : "projectDetails/:id" , element :<ProjectDetails/>},
-      {path : "resume" , element :<Resume/>},
-    ]}
-  ])
-
-  return <>
-
-  <div className="container container-color">
-  <RouterProvider router={routers}>
-  </RouterProvider>
-  </div>
-
-
-  
-  
-  </>
+  return (
+    <div className="container container-color">
+      <RouterProvider router={routers} />
+    </div>
+  );
 }
-  {/* <Header/>
-  <Hero/>
-  <Main/>
-  <ContactUs/>
-  <Footer/> */}
+
 export default App;
